76e5893b794eba0ac85566bed2201d46ff34fc78
97252 <lavyoud@gmail.com>
2025-01-24 15:07:54 +0200

    create student adapter connect adapter to the recycler view

--- app/src/main/java/com/example/studentslist/MainActivity.kt
+++ app/src/main/java/com/example/studentslist/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.studentslist
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_list)
+
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = StudentAdapter(mutableListOf())
+        recyclerView.adapter = adapter
+
+        StudentsRepository.students.observe(this) { students ->
+            adapter.updateStudents(students)
+        }
 
         findViewById<Button>(R.id.addStudentButton).setOnClickListener {
             startActivity(Intent(this, AddStudentActivity::class.java))
